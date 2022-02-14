af6b295b6d7599575fe42cd8a9ddc04846fca203
Supakarn <Yoojongdee_s@silpakorn.edu>
2022-02-15 00:49:38 +0700

    commit

--- spec.py
+++ spec.py
@@ -1,16 +1,15 @@
 
-def A(number) :
+def funcA(number) :
     text1 = str(number)
-    text = ""
+    result = ""
     for i in text1 :
         if( int(i) % 2 == 0 ) :
-            text += "Even" + i
+            result += "Even" + i
         else :
-            text += "Odd" + i
-    return text
+            result += "Odd" + i
+    return result
 
-
-def B(text) :
+def funcB(text) :
     check = ["Odd","Even"]
     text1 = ""
     result = ""
@@ -27,69 +26,71 @@ def B(text) :
             i += 1
     return result
 
-def C(text) :
-    text2 = ""
+def funcC(text) :
+    text1 = ""
     for i in text :
         if(ord(i) >= 48 and ord(i) <= 57) :
-            text2 += i
+            text1 += i
         else :
-            text2 += str(ord(i))
-    return text2
+            text1 += str(ord(i))
+    return text1
 
-def D(text) :
+def funcD(text) :
     check = ["DDO","NEVE"]
-    text2 = ""
-    rslt = ""
+    text1 = ""
+    result = ""
     i = 0
     while i < len(text) : 
-        if(text2 == check[0] or text2 == check[1]):        
-            text2 +=  str(text[i])
-            rslt += text2
-            text2 = ""
+        if(text1 == check[0] or text1 == check[1]):        
+            text1 +=  str(text[i])
+            result += text1
+            text1 = ""
             i += 1
         else :
-            text2 += chr(int(text[i:i+2]))
+            text1 += chr(int(text[i:i+2]))
             i += 2
-    return rslt
+    return result
 
-def E(text) :
+def funcE(text) :
     check = ["DDO","NEVE"]
-    text2 = ""
-    rslt = ""
+    text1 = ""
+    result = ""
     i = 0
     while i < len(text) : 
-        if(text2 == check[0] or text2 == check[1]):        
-            text2 =  text2[::-1]
-            text2 = text2[0].upper() + text2[1:].lower()
-            text2 += text[i]
-            rslt += text2
-            text2 = ""
+        if(text1 == check[0] or text1 == check[1]):        
+            text1 =  text1[::-1]
+            text1 = text1[0].upper() + text1[1:].lower()
+            text1 += text[i]
+            result += text1
+            text1 = ""
             i += 1
         else :
-            text2 += text[i]
+            text1 += text[i]
             i += 1
+    return result
 
-    return rslt
-
-def F(text) :
-    rslt =""
+def funcF(text) :
+    result =""
     for i in text :
         if(ord(i) >= 48 and ord(i) <= 57 ) :
-            rslt += i
-    return rslt 
+            result += i
+    return result 
 
 
 target = int(input("Enter number : "))
-print("Input is" ,target)
-A = A(target)
-print("Funtion A :" ,A)
-B = B(A)
-print("Funtion B :" ,B)
-C = C(B)
-print("Funtion C :" ,C)
-D = D(C)
-print("Funtion D :" ,D)
-E = E(D)
-print("Funtion E :" ,E)
-F = F(E)
-print("Funtion F :" ,F)
+
+if(target > 0) :
+    A = funcA(target)
+    print("Funtion A :" ,A)
+    B = funcB(A)
+    print("Funtion B :" ,B)
+    C = funcC(B)
+    print("Funtion C :" ,C)
+    D = funcD(C)
+    print("Funtion D :" ,D)
+    E = funcE(D)
+    print("Funtion E :" ,E)
+    F = funcF(E)
+    print("Funtion F :" ,F)
+else :
+    print("Invalid Input")
